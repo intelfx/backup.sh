6715e3f8827785dbac823728b673f9a463c77846
Ivan Shapovalov <intelfx@intelfx.name>
2020-01-07 07:59:12 +0300

    btrfs_{delete,cleanup}.sh: add a few more scripts for working with btrfs snapshots

---
+++ btrfs_cleanup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -e
+
+. ${BASH_SOURCE%/*}/backup_lib.sh || exit
+
+#
+# config
+#
+
+(( $# >= 1 )) || die "btrfs_cleanup.sh: bad arguments ($*): expecting <config>"
+CONFIG="$1"
+shift 1
+
+load_config "$CONFIG" "$@"
+
+#
+# main
+#
+
+log "Cleaning up obsolete subvolumes after restoring a snapshot for filesystem '$FILESYSTEM'"
+
+MOUNT_DIR="$(mktemp -d)"
+cleanup_add "rm -rf '$MOUNT_DIR'"
+
+btrfs_remount_id5_to "$FILESYSTEM" "$MOUNT_DIR"
+cleanup_add "umount -l '$MOUNT_DIR'"
+
+OLD_DIR="$MOUNT_DIR/old"
+if ! [[ -d "$OLD_DIR" ]]; then
+	warn "No old snapshots to delete: $OLD_DIR is not a directory"
+	return 0
+fi
+
+SUBVOLUMES_LIST_CMD=(
+	btrfs-sub-find --find
+	"$OLD_DIR"
+)
+
+< <( "${SUBVOLUMES_LIST_CMD[@]}" | sort -r ) readarray -t SUBVOLUMES
+
+for s in "${SUBVOLUMES[@]}"; do
+	log "Will delete snapshot '$s'"
+done
+
+btrfs sub del --verbose --commit-after "${SUBVOLUMES[@]}"
---
+++ btrfs_delete.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -e
+
+. ${BASH_SOURCE%/*}/backup_lib.sh || exit
+
+#
+# config
+#
+
+(( $# >= 2 )) || die "btrfs_cleanup.sh: bad arguments ($*): expecting <config> <snapshot name>"
+CONFIG="$1"
+SNAPSHOT_NAME="$2"
+shift 2
+
+load_config "$CONFIG" "$@"
+
+SNAPSHOT_PATH="$(btrfs_snapshot_path "$SNAPSHOT_NAME")"
+
+#
+# main
+#
+
+log "Deleting snapshot tree for filesystem '$FILESYSTEM' at '$SNAPSHOT_PATH'"
+
+MOUNT_DIR="$(mktemp -d)"
+cleanup_add "rm -rf '$MOUNT_DIR'"
+
+btrfs_remount_id5_to "$FILESYSTEM" "$MOUNT_DIR"
+cleanup_add "umount -l '$MOUNT_DIR'"
+
+SNAPSHOT_DIR="$MOUNT_DIR/$SNAPSHOT_PATH"
+if ! [[ -d "$SNAPSHOT_DIR" ]]; then
+	die "Bad snapshot dir: $SNAPSHOT_DIR (name: $SNAPSHOT_NAME)"
+fi
+
+SUBVOLUMES_LIST_CMD=(
+	btrfs-sub-find --find
+	"$SNAPSHOT_DIR"
+)
+
+< <( "${SUBVOLUMES_LIST_CMD[@]}" | sort -r ) readarray -t SUBVOLUMES
+
+for s in "${SUBVOLUMES[@]}"; do
+	log "Will delete snapshot '$s'"
+done
+
+if (( ${#SUBVOLUMES[@]} )); then
+	btrfs sub del --verbose --commit-after "${SUBVOLUMES[@]}"
+else
+	warn "No subvolumes to delete for '$SNAPSHOT_NAME' -- empty snapshot tree?"
+fi
+
+rm -vrf "$SNAPSHOT_DIR"
