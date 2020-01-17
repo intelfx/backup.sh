c171d210603f988d3d8e6c8a164f970265fdf31e
Ivan Shapovalov <intelfx@intelfx.name>
2020-01-17 04:29:36 +0300

    backup_lib_prune.sh: factor out backup list loading and trial from backup_prune.sh

--- backup_lib_prune.sh
+++ backup_lib_prune.sh
@@ -227,7 +227,7 @@ prune_try_backup() {
 
 	# only the first matched rule is used to generate a verdict, but we still run all rules to update their state
 	local rule verdict="" verdict_rule=""
-	for rule in "${PRUNE_RULES[@]}"; do
+	for rule in "$@"; do
 		prune_try_rule
 	done
 
@@ -246,4 +246,24 @@ prune_try_backup() {
 		die "bad verdict: $snap = $verdict"
 		;;
 	esac
+}
+
+prune_load_backups() {
+	while read snap; do
+		# assume that snapshot IDs are valid (ISO 8601) timestamps
+		snap_epoch="$(epoch "$snap")"
+		BACKUPS+=( "$snap_epoch $snap" )
+	done < <( "$@" )
+
+	# backups are tried recent-first, as this aligns with daily/weekly/monthly rule semantics
+	# (that is, keep the most recent backup in a given timeframe)
+	# TODO: might want to implement configurable order
+	sort_array BACKUPS -r -n -k1
+}
+
+prune_try_backups() {
+	for line in "${BACKUPS[@]}"; do
+		read snap_epoch snap <<< "$line"
+		prune_try_backup "$@"
+	done
 }
--- backup_prune.sh
+++ backup_prune.sh
@@ -22,26 +22,13 @@ load_config "$CONFIG" "$@"
 log "pruning backups using ${#PRUNE_RULES[@]} rule(s) in $CONFIG"
 
 BACKUPS=()
+prune_load_backups "${PRUNE_LIST[@]}"
+
 PRUNE=()
 prune_callback() {
 	PRUNE+=( "$@" )
 }
-
-while read snap; do
-	# assume that snapshot IDs are valid (ISO 8601) timestamps
-	snap_epoch="$(epoch "$snap")"
-	BACKUPS+=( "$snap_epoch $snap" )
-done < <("${PRUNE_LIST[@]}")
-
-# backups are tried recent-first, as this aligns with daily/weekly/monthly rule semantics
-# (that is, keep the most recent backup in a given timeframe)
-# TODO: might want to implement configurable order
-sort_array BACKUPS -r -n -k1
-
-for line in "${BACKUPS[@]}"; do
-	read snap_epoch snap <<< "$line"
-	prune_try_backup
-done
+prune_try_backups "${PRUNE_RULES[@]}"
 
 if (( ${#PRUNE[@]} )); then
 	log "pruning ${#PRUNE[@]} backup(s)"
