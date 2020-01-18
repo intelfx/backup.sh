130214adfe6cefd8519e6863659cca87f51abd3e
Ivan Shapovalov <intelfx@intelfx.name>
2020-01-18 04:55:16 +0300

    cfg_rules_common.sh: retain 30 daily snapshots in longterm storage

--- cfg_rules_common.sh
+++ cfg_rules_common.sh
@@ -3,8 +3,7 @@
 RULES_LONGTERM=(
 	"keep_minutely every=15 count=1 minutes=$(( 8*60 ))" # 8 hours
 	"keep_hourly count=1 hours=24"
-	"keep_daily count=1 days=7"
-	"keep_weekly count=1 weeks=4"
+	"keep_daily count=1 days=30"
 	"keep_monthly count=1 months=12"
 	"keep_yearly count=1 years=10"
 	delete=1
