24dfbbcbc86b026d3d2b807a67056a183a6c0567
Ivan Shapovalov <intelfx@intelfx.name>
2020-01-23 10:13:07 +0300

    backup_lib.sh: add log_array() to print an array using log()

--- backup_lib.sh
+++ backup_lib.sh
@@ -118,6 +118,16 @@ function label() {
 	fi
 }
 
+function log_array() {
+	local fun="$1" title="$2"
+	shift 2
+	"$fun" "$title: $#"
+	local arg
+	for arg; do
+		log "- $arg"
+	done
+}
+
 function epoch() {
 	local time="$1"
 	[[ "$time" ]] || die "epoch: empty timestamp passed"
