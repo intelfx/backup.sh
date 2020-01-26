41d006d6da82f7e7e27785e97d81a4edcf550048
Ivan Shapovalov <intelfx@intelfx.name>
2020-01-26 07:00:02 +0300

    backup.sh: load backup_lib.sh from current location

--- backup.sh
+++ backup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash -e
 
-. lib.sh || exit
+. ${BASH_SOURCE%/*}/backup_lib.sh || exit
 
 cd "${BASH_SOURCE%/*}"
 
 log "Snapshotting btrfs"
-./backup_schedule.sh cfg_btrfs_schedule.sh
+backup_schedule.sh cfg_btrfs_schedule.sh
 
 log "Pushing btrfs snapshots to borg"
-./backup_consume.sh cfg_consume_btrfs_borg_system.sh
+backup_consume.sh cfg_consume_btrfs_borg_system.sh
 
 log "Cleaning up"
-./backup_prune.sh cfg_btrfs_prune.sh
-./backup_prune.sh cfg_borg_system_prune.sh
+backup_prune.sh cfg_btrfs_prune.sh
+backup_prune.sh cfg_borg_system_prune.sh
