afca1dc197d82cbaf96bbdffddf6a00b86584b78
Ivan Shapovalov <intelfx@intelfx.name>
2023-10-08 20:54:19 +0400

    btrfs: list: drop redundant escaping

--- engines/btrfs/list.sh
+++ engines/btrfs/list.sh
@@ -35,7 +35,7 @@ SNAPSHOT_GLOB="'$MOUNT_DIR/$(btrfs_snapshot_path "'*'")'"
 (shopt -s nullglob; eval "print_array $SNAPSHOT_GLOB") | readarray -t SNAPSHOT_PATHS
 
 SNAPSHOT_ID_REGEX="^$MOUNT_DIR/$(btrfs_snapshot_path "([^/]+)")$"
-print_array "${SNAPSHOT_PATHS[@]}" | sed -nr "s|$SNAPSHOT_ID_REGEX|\\1|p" | readarray -t SNAPSHOT_IDS
+print_array "${SNAPSHOT_PATHS[@]}" | sed -nr "s|$SNAPSHOT_ID_REGEX|\1|p" | readarray -t SNAPSHOT_IDS
 
 label "Btrfs snapshots:"
 print_array "${SNAPSHOT_IDS[@]}"
