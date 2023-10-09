a9bd7c60d6b9e3a01b78252ae5cc03fda3d4c089
Ivan Shapovalov <intelfx@intelfx.name>
2023-10-09 21:49:38 +0400

    libbackup: do not play games when setting log prefix

--- libbackup/libbackup.sh.d/init.sh
+++ libbackup/libbackup.sh.d/init.sh
@@ -3,7 +3,7 @@
 export BSH_ROOT_DIR="$(realpath -qm --strip "$BASH_SOURCE/../../..")"
 export PATH="$BSH_ROOT_DIR/lib:$PATH"
 
-LIBSH_LOG_PREFIX="$(realpath -qe --strip --relative-to="$BSH_ROOT_DIR" "${BASH_SOURCE[2]}")"
+LIBSH_LOG_PREFIX="$(basename "${BASH_SOURCE[2]}")"
 
 VERB_DIR="$BSH_ROOT_DIR/cmds"
 engine_verb_dir() {
