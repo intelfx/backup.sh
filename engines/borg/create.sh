30b61874a94a830fbde10171182e9a81d8b6f11d
Ivan Shapovalov <intelfx@intelfx.name>
2023-10-09 18:48:11 +0400

    borg: create: rework

--- engines/borg/create.sh
+++ engines/borg/create.sh
@@ -1,20 +1,35 @@
-#!/bin/bash -e
+#!/hint/bash
 
-. ${BASH_SOURCE%/*}/backup_lib.sh || exit
-. borg_lib.sh || exit
+#
+# options
+#
+
+_usage() {
+	cat <<EOF
+$_usage_common_syntax create <JOB> [SNAPSHOT-NAME]
+$_usage_common_options
+create options:
+	SNAPSHOT-NAME		Optional name of the source snapshot to use
+				(if unspecified, config default is used)
+EOF
+}
+
+__verb_expect_args_one_of 1 2
+SNAPSHOT_ID=( "${VERB_ARGS[@]:1}" )
 
 
 #
 # config
 #
 
-(( $# >= 1 )) || die "bad arguments ($*): expecting <config>"
-CONFIG="$1"
-shift 1
+config_get_job "$JOB_NAME" SOURCE_JOB_NAME BORG_REPO
+config_get_job_f "$JOB_NAME" borg_snapshot_id borg_snapshot_tag borg_exports
+BORG_MOUNT_DIR="/tmp/borg"
+borg_exports
 
-load_config "$CONFIG" "$@"
-
-SNAPSHOT_ID="$(borg_snapshot_id)"
+if ! [[ ${SNAPSHOT_ID+set} ]]; then
+	SNAPSHOT_ID="$(borg_snapshot_id)"
+fi
 SNAPSHOT_TAG="$(borg_snapshot_tag "$SNAPSHOT_ID")"
 
 
@@ -39,7 +54,7 @@ if ! mkdir "$BORG_MOUNT_DIR"; then
 	die "cannot create stable mountpoint '$BORG_MOUNT_DIR' -- already exists?"
 fi
 cleanup_add "invoke unmount '$BORG_MOUNT_DIR'"
-invoke mount "${SOURCE[@]}" "$SNAPSHOT_ID" "$BORG_MOUNT_DIR"
+invoke mount "$SOURCE_JOB_NAME" "$SNAPSHOT_ID" "$BORG_MOUNT_DIR"
 
 pushd "$BORG_MOUNT_DIR" &>/dev/null
 cleanup_add "popd &>/dev/null"
